c1620f47509a45b9a5a353bb44676a3bf83ce1a0
sarmaakondi <avnmsarma@gmail.com>
2024-11-07 20:36:48 +1100

    Update vite config to optimize the production build

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     base: "/bin2dec/",
+    optimizeDeps: {
+        include: ["react", "react-dom"],
+    },
+    build: {
+        sourcemap: false,
+    },
 });
