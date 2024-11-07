cb46f3683b73ae56c61ba241f98b2dcac88057d6
sarmaakondi <avnmsarma@gmail.com>
2024-11-07 17:36:19 +1100

    Add logo icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import { Box, TextField, Typography } from "@mui/material";
+import LogoIcon from "../public/bin2code_xl.png";
 
 export default function App() {
     return (
         <Container maxWidth="sm">
-            <Typography variant="h3" align="center" mt={4}>
-                Bin2Dec
-            </Typography>
+            <Box
+                mt={4}
+                display="flex"
+                alignItems="center"
+                justifyContent="space-around"
+            >
+                <img src={LogoIcon} alt="logo icon" style={{ width: 100 }} />
+                <Typography variant="h1" align="center">
+                    Bin2Dec
+                </Typography>
+            </Box>
+
             <Box mt={4}>
                 <TextField
                     label="Binary"
