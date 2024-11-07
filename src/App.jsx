4ef72c98c5b050a96e81b31008f1bddad9cee701
sarmaakondi <avnmsarma@gmail.com>
2024-11-07 21:01:45 +1100

    Optimization of imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Container from "@mui/material/Container";
-import { Box, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import LogoIcon from "../public/bin2code_md.png";
 import BinaryInput from "./BinaryInput";
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
 import { createRoot } from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import App from "./App";
 import theme from "./theme";
-import { StrictMode } from "react";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-    <StrictMode>
-        <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <App />
-        </ThemeProvider>
-    </StrictMode>
+    <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <App />
+    </ThemeProvider>
 );
