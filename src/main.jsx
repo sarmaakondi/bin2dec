fc3197d39a51ebe6ab7a87bb130cc538c4913767
sarmaakondi <avnmsarma@gmail.com>
2024-11-07 20:26:40 +1100

    Fix issue with StrictMode import

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,17 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import theme from "./theme";
+import { StrictMode } from "react";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-    <React.StrictMode>
+    <StrictMode>
         <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
             <App />
         </ThemeProvider>
-    </React.StrictMode>
+    </StrictMode>
 );
